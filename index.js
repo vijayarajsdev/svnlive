ee1c8933bfef3e82a457e0253971f1a4f42b43c2
vijayarajsdev <vijayarajsdev@gmail.com>
2023-09-16 22:10:09 +0530

    added functional enquiry form

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-
+(function() {
+    
+    emailjs.init('spfKtQYK32ZwwTd8C');
+})();
 function navhandler(){
     let a=document.getElementById("Navlist");
     if(a.style.display==="block"){
@@ -18,3 +21,34 @@ function closenavmenu() {
 }
 
 
+const formhandler=(e)=>{
+    e.preventDefault();
+    const form=document.getElementById('form');
+const name=document.getElementById('name').value;
+const number=document.getElementById('number').value;
+const subject=document.getElementById('subject').value;
+const mail=document.getElementById('email').value;
+const message=document.getElementById('message').value;
+if(name===''||number===''||subject===''||mail===''||message===''){
+    document.getElementById('formvalidation').innerHTML='please enter all fields'
+}
+
+var templateParams = {
+    name: name,
+    mail: mail,
+    message:message,
+    subject:subject,
+    number:number
+};
+ 
+emailjs.send('service_yske6th', 'template_vv25ztr', templateParams)
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
+console.log(templateParams);
+form.reset();
+
+}
+form.addEventListener("submit",formhandler)
