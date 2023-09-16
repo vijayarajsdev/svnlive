9500eef0215dabdc14b0db492b2b1016e0121d7a
vijayarajsdev <vijayarajsdev@gmail.com>
2023-09-16 15:28:36 +0530

    version 1 with head and foot

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function navhandler(){
+    let a=document.getElementById("Navlist");
+    if(a.style.display==="block"){
+        a.style.display="none";
+    }
+    else{
+        a.style.display="block";
+    }
+}
+
+function closenavmenu() {
+    let b=document.getElementById("Navlist");
+    if(b.style.display==="block"){
+        b.style.display="none";
+    }
+    
+}
+
+
